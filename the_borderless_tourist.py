ebe6d7b4d7bda61c4166d550da479c39174a4bdb
jkhissa <jkhissa@gmail.com>
2022-11-23 22:37:19 +0000

    Created attractions and functionality for adding new attractions

--- the_borderless_tourist.py
+++ the_borderless_tourist.py
@@ -13,7 +13,33 @@ def get_traveler_location(traveler):
 
 test_destination_index = get_traveler_location(test_traveler)
 
-print(test_destination_index)
-print(get_destination_index("Los Angeles, USA"))
-print(get_destination_index("Paris, France"))
+# print(test_destination_index)
+# print(get_destination_index("Los Angeles, USA"))
+# print(get_destination_index("Paris, France"))
 # print(get_destination_index("Hyderabad, India"))
+
+attractions = [[] for i in destinations]
+print(attractions)
+
+def add_attraction(destination, attraction):
+    destionation_index = get_destination_index(destination)
+    attractions_for_destionations = attractions[destionation_index]
+    attractions_for_destionations.append(attraction)
+    return attractions_for_destionations
+
+add_attraction('Los Angeles, USA',['Venice Beach', ['beach']])
+print(attractions)
+
+add_attraction("Paris, France", ["the Louvre", ["art", "museum"]])
+add_attraction("Paris, France", ["Arc de Triomphe", ["historical site", "monument"]])
+add_attraction("Shanghai, China", ["Yu Garden", ["garden", "historical site"]])
+add_attraction("Shanghai, China", ["Yuz Museum", ["art", "museum"]])
+add_attraction("Shanghai, China", ["Oriental Pearl Tower", ["skyscraper", "viewing deck"]])
+add_attraction("Los Angeles, USA", ["LACMA", ["art", "museum"]])
+add_attraction("São Paulo, Brazil", ["São Paulo Zoo", ["zoo"]])
+add_attraction("São Paulo, Brazil", ["Pátio do Colégio", ["historical site"]])
+add_attraction("Cairo, Egypt", ["Pyramids of Giza", ["monument", "historical site"]])
+add_attraction("Cairo, Egypt", ["Egyptian Museum", ["museum"]])
+
+print(attractions)
+
