3281d727a84d3c3d42f692bfd0e63f2dac203805
jkhissa <jkhissa@gmail.com>
2023-08-14 16:40:17 +0100

    starting new budget Budget_app

--- dictionairy.py
+++ dictionairy.py
@@ -7,4 +7,27 @@ plays["Purple Haze"] = 1
 plays["Respect"] = 94
 
 library = {"The Best Songs": plays, "Sunday Feelings": {}}
-print(library)
+# print(library)
+
+letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+points = [1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 4, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10]
+
+letter_to_points = dict(zip(letters, points))
+
+# print(letter_to_points)
+
+letter_to_points.update({' ': 0})
+
+# print(letter_to_points)
+
+def score_word(word):
+    point_total = 0
+    for char in word:
+        point_total += letter_to_points.get(char, 0)
+    print(point_total)
+    
+brownie_points = score_word('BROWNIE')
+print(brownie_points)
+
+player_to_words = {"player1":["BLUE","TENNIS","EXIT"],"wordNerd":["EARTH","EYES","MACHINE"],"Lexi Con":["ERASER","BELLY","HUSKY"],"Prof Reader":["ZAP","COMA","PERIOD"]}
+print(player_to_words)
